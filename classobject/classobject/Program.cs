810124bd4705b59db5601bb804b725acf5073b2e
Aysu <aysumamtiyeva5@hotmail.com>
2026-03-16 23:45:12 +0400

    task was added

---
+++ classobject/classobject/Program.cs
@@ -0,0 +1,64 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student("Ali", "Mammadov", 20, "ali@mail.com", "P001", "ST1001", "IT", 88.5, 3);
+
+        Student s2 = new Student("Nigar", "Aliyeva", 21, "nigar@mail.com", "P002", "ST1002", "IT", 92.0, 4);
+
+        Student s3 = new Student("Murad", "Huseynov", 19, "murad@mail.com", "P003", "ST1003", "IT", 68.5, 2);
+
+
+      
+        Teacher t1 = new Teacher("Rashad", "Karimov", 40, "rashad@mail.com", "T001", "Computer Science", "Programming", 800, 15);
+
+        Teacher t2 = new Teacher("Leyla", "Hasanova", 35, "leyla@mail.com", "T002", "Computer Science", "Databases", 800, 8);
+
+
+        Administrator admin = new Administrator("Kamran", "Aliyev", 45, "kamran@mail.com", "A001", "Dekan", "IT Faculty", 5);
+
+
+        double totalScholarship = 0;
+        decimal totalSalary = 0;
+
+
+      
+        s1.ShowStudentInfo();
+        Console.WriteLine("Təqaüd: " + s1.CalculateScholarship());
+        totalScholarship += s1.CalculateScholarship();
+        Console.WriteLine();
+
+        s2.ShowStudentInfo();
+        Console.WriteLine("Təqaüd: " + s2.CalculateScholarship());
+        totalScholarship += s2.CalculateScholarship();
+        Console.WriteLine();
+
+        s3.ShowStudentInfo();
+        Console.WriteLine("Təqaüd: " + s3.CalculateScholarship());
+        totalScholarship += s3.CalculateScholarship();
+        Console.WriteLine();
+
+
+        t1.ShowTeacherInfo();
+        Console.WriteLine("Maaş: " + t1.CalculateSalary());
+        totalSalary += t1.CalculateSalary();
+        Console.WriteLine();
+
+        t2.ShowTeacherInfo();
+        Console.WriteLine("Maaş: " + t2.CalculateSalary());
+        totalSalary += t2.CalculateSalary();
+        Console.WriteLine();
+
+
+        admin.ShowAdminInfo();
+        Console.WriteLine();
+
+      
+        admin.GrantAccess(s1);
+
+        Console.WriteLine();
+        Console.WriteLine(" Statistika");
+        Console.WriteLine("Ümumi təqaüd xərci: " + totalScholarship);
+        Console.WriteLine("Ümumi maaş xərci: " + totalSalary);
+    }
+}
---
+++ classobject/classobject/student.cs
@@ -0,0 +1,39 @@
+﻿public class Student : Person
+{
+    public string StudentNumber;
+    public string Faculty;
+    public double GPA;
+    public int Year;
+
+    public Student(string firstName, string lastName, int age, string email, string id,
+                   string studentNumber, string faculty, double gpa, int year)
+        : base(firstName, lastName, age, email, id)
+    {
+        this.StudentNumber = studentNumber;
+        this.Faculty = faculty;
+        this.GPA = gpa;
+        this.Year = year;
+    }
+
+    public void ShowStudentInfo()
+    {
+        Console.WriteLine("Tələbə Məlumatı ");
+        ShowBasicInfo();
+        Console.WriteLine("Tələbə nömrəsi: " + StudentNumber);
+        Console.WriteLine("Fakültə: " + Faculty);
+        Console.WriteLine("Kurs: " + Year);
+        Console.WriteLine("GPA: " + GPA);
+    }
+
+    public double CalculateScholarship()
+    {
+        if (GPA >= 90)
+            return 500;
+        else if (GPA >= 80)
+            return 350;
+        else if (GPA >= 70)
+            return 200;
+        else
+            return 0;
+    }
+}
