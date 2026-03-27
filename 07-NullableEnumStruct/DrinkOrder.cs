212a9b79295c661c1ce82ff1c7d64ca8280bc6f9
Aysu <aysumamtiyeva5@hotmail.com>
2026-03-27 18:57:36 +0400

    task was added

---
+++ 07-NullableEnumStruct/DrinkOrder.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_NullableEnumStruct
+{
+    enum DrinkType
+    {
+        Coffee,
+        Juice,
+        Water,
+        Tea
+    }
+    enum DrinkSize
+    {
+        Medium,
+        Small,
+        Large
+    }
+    enum  OrderStatus {
+
+        New,
+        Preparing,
+        Ready,
+        Delivered
+    }
+    internal class DrinkOrder
+    {
+        public int OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public DrinkType Drink { get; set; }
+        public DrinkSize Size { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal Price { get; set; }
+
+    public DrinkOrder (int orderNumber, string customerName, DrinkType drink, DrinkSize size)
+    {
+        OrderNumber = orderNumber;
+        CustomerName = customerName;
+        Drink = drink;
+        Size = size;
+        Status = OrderStatus.New;
+        Price = CalculatePrice();
+    }
+    public decimal CalculatePrice ()
+    {
+        if (Drink == DrinkType.Coffee)
+
+        {
+            if (Size == DrinkSize.Small)
+                return 3;
+            else if (Size == DrinkSize.Medium)
+                return 4;
+            else if (Size == DrinkSize.Large)
+                return 5;
+        }
+        else if (Drink == DrinkType.Juice)
+        {
+            if (Size == DrinkSize.Small)
+                return 4;
+            else if (Size == DrinkSize.Medium)
+                return 5;
+            else if (Size == DrinkSize.Large)
+                return 6;
+        }
+        else if (Drink == DrinkType.Water)
+        {
+            if (Size == DrinkSize.Small)
+                return 1;
+            else if (Size == DrinkSize.Medium)
+                return 1.5m;
+            else if (Size == DrinkSize.Large)
+                return 2;
+        }
+            else if (Drink == DrinkType.Tea)
+            {
+                if (Size == DrinkSize.Small)
+                    return 2;
+                else if (Size == DrinkSize.Medium)
+                    return 3;
+                else if (Size == DrinkSize.Large)
+                    return 4;
+            }
+            return 0;
+        }
+    }
+    
+}
---
+++ 07-NullableEnumStruct/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _07_NullableEnumStruct
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+
+            DrinkOrder order1 = new DrinkOrder(
+                1,
+                "Ali",
+                DrinkType.Coffee,
+                DrinkSize.Small
+            );
+
+
+            DrinkOrder order2 = new DrinkOrder(
+                2,
+                "Vuqar",
+                DrinkType.Juice,
+                DrinkSize.Medium
+            );
+
+
+            DrinkOrder order3 = new DrinkOrder(
+                3,
+                "Nicat",
+                DrinkType.Water,
+                DrinkSize.Large
+            );
+
+
+            DrinkOrder order4 = new DrinkOrder(
+                4,
+                "Leyla",
+                DrinkType.Tea,
+                DrinkSize.Medium
+            );
+
+
+
+
+            order1.Status = OrderStatus.Preparing;
+            order2.Status = OrderStatus.Ready;
+
+            Console.WriteLine("Status deyisenden sonra");
+            PrintOrder(order1);
+            PrintOrder(order2);
+            PrintOrder(order3);
+            PrintOrder(order4);
+
+        }
+
+        static void PrintOrder(DrinkOrder order)
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("Order Number: " + order.OrderNumber);
+            Console.WriteLine("Customer Name: " + order.CustomerName);
+            Console.WriteLine("Drink Type: " + order.Drink);
+            Console.WriteLine("Drink Size: " + order.Size);
+            Console.WriteLine("Status: " + order.Status);
+            Console.WriteLine("Price: " + order.Price);
+        }
+    }
+}
