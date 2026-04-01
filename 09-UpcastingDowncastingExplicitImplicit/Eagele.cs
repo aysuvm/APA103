d61af5ed87ddf575548f40ab4cc682f1b84d836a
aysuvm <aysuvm-apa103@code.edu.az>
2026-04-01 05:12:20 +0400

    task was added

---
+++ 09-UpcastingDowncastingExplicitImplicit/Eagele.cs
@@ -0,0 +1,13 @@
+﻿
+
+////namespace _09_UpcastingDowncastingExplicitImplicit
+//{
+   // internal class Eagele:Animal
+   // {
+      //  public int FlySpeed { get; set; }
+       /// public void Fly()
+       // {
+       //     Console.WriteLine("flied away");
+       // }
+   // }
+//}
