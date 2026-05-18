9981f903be79d2bae16bee4ba6d2774ca3b279bd
aysuvm <aysuvm-apa103@code.edu.az>
2026-05-18 22:10:11 +0400

    mvc

---
+++ 27-FrontToBackSqlConnection/Area/AdminPanel/Category/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using _27_FrontToBackSqlConnection.Data;
+using _27_FrontToBackSqlConnection.Models;
+using _27_FrontToBackSqlConnection.ViewModels;
+using FrontToBack.Areas.AdminPanel.ViewModels.Product;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontToBack.Areas.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public ProductController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Product> products = await _context.Products
+                .Where(p => !p.IsDeleted)
+                .Include(p => p.ProductImages)
+                .Include(p => p.Category)
+                .ToListAsync();
+
+            List <ProductsGetVM> productsGetVMs = new();
+
+            List<ProductGetVM> model = new();
+
+            foreach (var product in products)
+            {
+                ProductGetVM productGetVM = new()
+                {
+                    Name = product.Name,
+                    Price = product.Price,
+                    CategoryName = product.Category.Name,
+                    SKU = product.SKU,
+                    Image = product.ProductImages.FirstOrDefault()?.Image
+                };
+
+               
+            }
+
+            return View(products);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            ProductCreateVM productCreateVM = new()
+            {
+                Product  = await _context.Product.Where(c => !c.IsDeleted).ToListAsync()
+            };
+
+            return View(productCreateVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductCreateVM productCreateVM)
+        {
+            productCreateVM.Product = await _context.Product
+                .Where(c => !c.IsDeleted)
+                .ToListAsync();
+
+            if (!ModelState.IsValid)
+                return View(productCreateVM);
+
+            bool existCategory = productCreateVM.Product
+                .Any(c => c.Id == productCreateVM.CategoryId);
+
+            if (!existCategory)
+            {
+                ModelState.AddModelError(nameof(ProductCreateVM.CategoryId),
+                    "Category does not exist!");
+
+                return View(productCreateVM);
+            }
+
+            return View(productCreateVM);
+        }
+    }
+}
---
+++ 27-FrontToBackSqlConnection/ViewModels/ProductCreateVM.cs
@@ -0,0 +1,15 @@
+﻿namespace FrontToBack.Areas.AdminPanel.ViewModels.Product
+{
+    public class ProductCreateVM
+    {
+        public string? Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? SKU { get; set; }
+
+        public string? CategoryId { get; set; }
+    }
+}
